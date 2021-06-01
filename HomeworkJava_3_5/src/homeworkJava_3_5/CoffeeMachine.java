4f87d44a256cd115820a975441ffd580f7deab09
mihaipopa0107 <mihaipopa0107@gmail.com>
2021-06-01 19:36:26 +0300

    dependency_inversion

---
+++ HomeworkJava_3_5/src/homeworkJava_3_5/CoffeeMachine.java
@@ -0,0 +1,5 @@
+package homeworkJava_3_5;
+
+public interface CoffeeMachine {
+	public void start();
+}
---
+++ HomeworkJava_3_5/src/homeworkJava_3_5/SimpleCoffeeMachine.java
@@ -0,0 +1,7 @@
+package homeworkJava_3_5;
+
+public class SimpleCoffeeMachine implements CoffeeMachine {
+	public void start() {
+		System.out.println("Simple Coffee Machine started!");
+	}
+}
